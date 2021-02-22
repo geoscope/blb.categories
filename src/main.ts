405cca461439f5a6757816320a5ddb2c0e7d0951
Dominik <dmoreitz@gmail.com>
2021-02-22 17:39:38 +1100

    Minor logging cleanup

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     const listenPort = 3000;
-    console.log(`Starting listender on port ${listenPort}`);
     await app.listen(listenPort);
 }
 bootstrap();
--- src/storeid.middleware.ts
+++ src/storeid.middleware.ts
@@ -1,13 +1,14 @@
-import { Injectable, NestMiddleware } from '@nestjs/common';
+import { Logger, Injectable, NestMiddleware } from '@nestjs/common';
 
 @Injectable()
 export class StoreIdMiddleware implements NestMiddleware {
+    private readonly logger = new Logger(process.env.SERVICE_NAME);
+
     use(req: Request, res: Response, next: () => void) {
-        console.log(`Request: ${JSON.stringify(req.headers)}`);
         if (!req.headers['x-store-id']) {
             req.headers['x-store-id'] = process.env.DEFAULT_STORE_ID;
         }
-        console.debug(`x-store-id is ${req.headers['x-store-id']}`);
+        this.logger.debug(`x-store-id is ${req.headers['x-store-id']}`);
         next();
     }
 }
